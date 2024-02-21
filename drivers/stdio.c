74b9f12fe274e57fee163484a3e05b50201e55dd
Jacker <2103471429@qq.com>
2024-02-21 11:27:08 +0800

    修改bug,加入number这个静态函数

--- drivers/stdio.c
+++ drivers/stdio.c
@@ -1,6 +1,7 @@
 #include "console.h"
 #include "types.h"
 #include "vargs.h"
+#include <stdio.h>
 
 int printf(const char *format, ...){
     va_list args;
@@ -14,6 +15,89 @@ int printf(const char *format, ...){
 	console_write(printed);
 
     return printed;
+}
+
+static char *number(char *str, int num, int base, int size, int precision, int type)
+{
+	char c, sign, tmp[36];
+	const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	if (type & SMALL) {
+		digits ="0123456789abcdefghijklmnopqrstuvwxyz";
+	}
+	if (type & LEFT) {
+		type &= ~ZEROPAD;
+	}
+	if (base < 2 || base > 36) {
+		return 0;
+	}
+
+	c = (type & ZEROPAD) ? '0' : ' ' ;
+
+	if (type & SIGN && num < 0) {
+		sign = '-';
+		num = -num;
+	} else {
+		sign = (type&PLUS) ? '+' : ((type&SPACE) ? ' ' : 0);
+	}
+
+	if (sign) {
+	      size--;
+	}
+	if (type & SPECIAL) {
+		if (base == 16) {
+			size -= 2;
+		} else if (base == 8) {
+			size--;
+		}
+	}
+	i = 0;
+	if (num == 0) {
+		tmp[i++] = '0';
+	} else {
+		while (num != 0) {
+			tmp[i++] = digits[do_div(num,base)];
+		}
+	}
+
+	if (i > precision) {
+		precision = i;
+	}
+	size -= precision;
+
+	if (!(type&(ZEROPAD+LEFT))) {
+		while (size-- > 0) {
+			*str++ = ' ';
+		}
+	}
+	if (sign) {
+		*str++ = sign;
+	}
+	if (type & SPECIAL) {
+		if (base == 8) {
+			*str++ = '0';
+		} else if (base == 16) {
+			*str++ = '0';
+			*str++ = digits[33];
+		}
+	}
+	if (!(type&LEFT)) {
+		while (size-- > 0) {
+			*str++ = c;
+		}
+	}
+	while (i < precision--) {
+		*str++ = '0';
+	}
+	while (i-- > 0) {
+		*str++ = tmp[i];
+	}
+	while (size-- > 0) {
+		*str++ = ' ';
+	}
+
+	return str;
 }
 
 int vsprintf(char *buff, const char *format, va_list args)
