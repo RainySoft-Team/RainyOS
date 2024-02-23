a25976d9f477552b264236ea34550e415a05aca1
Rainy101112 <136242076+Rainy101112@users.noreply.github.com>
2024-02-23 19:06:34 +0800

    Add files via upload (#8)

--- include/vargs.h
+++ include/vargs.h
@@ -1,10 +1,34 @@
 #ifndef INCLUDE_VARGS_H_
 #define INCLUDE_VARGS_H_
 
-typedef __builtin_va_list va_list;
+//typedef __builtin_va_list va_list;
 
-#define va_start(ap, last)         (__builtin_va_start(ap, last))
-#define va_arg(ap, type)           (__builtin_va_arg(ap, type))
-#define va_end(ap) 
+typedef char* va_list;
+
+//#define va_start(ap, last)         (__builtin_va_start(ap, last))
+//#define va_arg(ap, type)           (__crt_va_arg(ap, type))
+//#define va_end(ap) 
+
+
+#define __crt_va_arg(ap, t)                                               \
+        ((sizeof(t) > sizeof(__int64) || (sizeof(t) & (sizeof(t) - 1)) != 0) \
+            ? **(t**)((ap += sizeof(__int64)) - sizeof(__int64))             \
+            :  *(t* )((ap += sizeof(__int64)) - sizeof(__int64)))
+
+
+//-------------------------------------------------------------------
+void __cdecl __va_start(va_list* , ...);
+#define __crt_va_start_a(ap, x) ((void)(__va_start(&ap, x)))
+#define __crt_va_start(ap, x) __crt_va_start_a(ap, x)
+
+#define __crt_va_end(ap)        ((void)(ap = (va_list)0))
+//--------------------------------------------------------------------
+
+
+
+#define va_arg(ap, type)           (__crt_va_arg(ap, type))
+#define va_start __crt_va_start
+#define va_end   __crt_va_end
+//抄代码得抄完整，我的stdio.c不然都编译不了
 
 #endif 	// INCLUDE_VARGS_H_
