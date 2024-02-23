f9cfa700d94d15d043e96fd2afa376cbbb55edfa
Marker <126474869+sdfhdjsf@users.noreply.github.com>
2024-02-23 20:14:08 +0800

    Revert "修改bug (#9)"
    This reverts commit 721a17be46670bc1586b9f8c90bf22808ea7143d.

--- include/stdio.h
+++ include/stdio.h
@@ -1,19 +1,8 @@
 #ifndef _STDIO_H_
 #define _STDIO_H_
 
-#define LEFT 1
-#define RIGHT 0
-
 #include "stdarg.h"
-#include "vargs.h"
 
-// printf.c
-int sprintf(char *buf, const char *fmt, ...);
-int vprintf(const char *fmt, va_list args);
-int printf(const char *fmt, ...);
-int vsprintf(char *buffer,const char *format,va_list args);
-
-/*
 int open(const char *pathname, uint8_t flags);
 int close(int fd);
 int read(int fd, void *buf, int count);
@@ -22,9 +11,14 @@ int unlink(int fd);
 int putchar(int ch);
 int puts(const char *s);
 
+// printf.c
+int vsprintf(char *str, const char *format, va_list ap);
+int sprintf(char *buf, const char *fmt, ...);
+int vprintf(const char *fmt, va_list args);
+int printf(const char *fmt, ...);
+
 // kstdio.c
 int printk(const char *fmt, ...);
 int serial_print(const char *fmt, ...);
-*/
 
 #endif
