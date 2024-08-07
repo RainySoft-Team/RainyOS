1660ed0b1f9b952361eecfb3df5cb632f632ae01
Rainy101112 <136242076+Rainy101112@users.noreply.github.com>
2024-08-07 15:09:37 +0800

    Delete include directory

--- include/shell.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_SHELL_H
-#define INCLUDE_SHELL_H
-
-void shell();
-
-#endif
--- include/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_TIMER_H_
-#define INCLUDE_TIMER_H_
-
-#include "types.h"
-
-void init_timer(uint32_t frequency);
-
-#endif 	// INCLUDE_TIMER_H_
